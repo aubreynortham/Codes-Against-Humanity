42537209a9e02b7a1035cc73c90ce5d3ce0b4114
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-09-01 18:26:40 -0400

    entries are being accessed with ng submit and pulled into the html

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 (function(){
   angular
@@ -8,10 +8,25 @@
     CAHctrlFunction
   ])
 
-  function CAHctrlFunction(){
+  function CAHctrlFunction($scope){
     var vm = this;
     vm.TALKTOME = "PAY ATTENTION TO MEEEEEEEEEE?!?!"
     console.log("hello world, meet controller");
+
+    //data lives here
+    var cardData = [
+      {question: "question1", answer: "answer1"}, {question: "question2", answer: "answer2"}, {question: "question3", answer: "answer3"}
+    ]
+    //randomly fetch a data object, separate into Q variable and A variable
+    var currentCard = cardData[Math.floor(Math.random()*cardData.length)]
+    vm.currentQuestion = currentCard["question"];
+    vm.currentAnswer = currentCard["answer"];
+    vm.guesses = [];
+    vm.newGuess = '';
+    vm.sendGuess = function(){
+      // console.log($scope.vm.newGuess);
+      vm.guesses.push($scope.vm.newGuess);
+    }
   }
 
 }());
